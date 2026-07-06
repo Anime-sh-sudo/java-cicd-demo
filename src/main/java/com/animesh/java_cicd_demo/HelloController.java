d53f0150f8483795005fff41d3d8d48b5f341999
Animesh Srivastava <110097097+Anime-sh-sudo@users.noreply.github.com>
2026-07-06 12:44:48 +0530

    Initial commit of my project

---
+++ src/main/java/com/animesh/java_cicd_demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.animesh.java_cicd_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from DevOps CI/CD Project!";
+    }
+}
